419551f654cd0528ef3684f17520584d799eb67f
sbx_user1051 <sbx_user1051@169.254.25.205>
2019-03-31 23:33:12 +0000

    Sun Mar 31 23:33:12 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,12 +1,14 @@
 package befaster.solutions.CHK;
 
 interface CheckoutInterface {
-	String checkOut(String val);
+	int checkOut(String... val);
 }
 
 public class CheckoutSolution {
-	CheckoutInterface msg = x -> {return "Checkout by " + x ;};
+	CheckoutInterface msg = (String x...) -> {if(x.equalsIgnoreCase("A")) return 50;
+	};
     public String checkout(String skus) {
        return msg.checkOut(skus);
     }
 }
+
