ced4bfe861cbc6f22b5dedbdc0beb6da028acdf7
sbx_user1051 <sbx_user1051@169.254.25.205>
2019-03-31 22:46:11 +0000

    Sun Mar 31 22:46:11 UTC 2019

--- src/main/java/befaster/solutions/SUM/SumSolution.java
+++ src/main/java/befaster/solutions/SUM/SumSolution.java
@@ -1,17 +1,12 @@
 package befaster.solutions.SUM;
 
-import befaster.runner.SolutionNotImplementedException;
-
-interface SumSolutionInterface {
-	int sumVal(int x,int y);
-}
-
 public class SumSolution {
 
     public int compute(int x, int y) {
-        throw new SolutionNotImplementedException();
+       return x+y;
     }
 
 }
+
 
 
