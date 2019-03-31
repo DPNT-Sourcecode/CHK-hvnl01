1bbba423e542de8f7cbcfb14bf97760ce78c53bc
sbx_user1051 <sbx_user1051@169.254.25.205>
2019-03-31 22:34:38 +0000

    Sun Mar 31 22:34:38 UTC 2019

--- src/main/java/befaster/solutions/SUM/SumSolution.java
+++ src/main/java/befaster/solutions/SUM/SumSolution.java
@@ -1,6 +1,5 @@
 package befaster.solutions.SUM;
 
-import befaster.runner.SolutionNotImplementedException;
 import java.util.Scanner;
 
 interface SumSolutionInterface {
@@ -9,15 +8,15 @@ interface SumSolutionInterface {
 
 public class SumSolution {
 
-    public static void main(String args[]) {
-    	Scanner input = new Scanner(System.in);
+   public static void main(String args[]) {
+    	@SuppressWarnings("resource")
+		Scanner input = new Scanner(System.in);
     	System.out.println("Enter two integer ::");
     	int firstInt = input.nextInt();
     	int secondInt = input.nextInt();
     	SumSolutionInterface sumValue = (int a, int b) -> {return (a+b);};
-    	
-    	System.out.println("Sum of "+firstInt+" and" + secondInt+" is::"+sumValue.sumVal(5,6));
-    	
+    	System.out.println("Sum of "+firstInt+" and " + secondInt+" is::"+sumValue.sumVal(firstInt,secondInt));
     }
 
 }
+
