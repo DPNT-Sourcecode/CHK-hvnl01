0e0b380d47a266ddc5e38ec0469742ed3f25790e
sbx_user1051 <sbx_user1051@169.254.25.205>
2019-03-31 22:51:17 +0000

    Sun Mar 31 22:51:17 UTC 2019

--- src/main/java/befaster/solutions/SUM/SumSolution.java
+++ src/main/java/befaster/solutions/SUM/SumSolution.java
@@ -1,9 +1,14 @@
 package befaster.solutions.SUM;
 
+interface SumInterface {
+	int sumVal(int x, int y);
+}
 public class SumSolution {
 
     public int compute(int x, int y) {
-       return x+y;
+       SumInterface val = (int a, int b)->{return a+b;};
+       return val.sumVal(x, y);
     }
 
 }
+
