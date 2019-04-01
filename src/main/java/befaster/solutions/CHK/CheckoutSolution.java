1469b2246f98f835a90b257a4f80f0ca46006d5a
sbx_user1051 <sbx_user1051@169.254.25.205>
2019-04-01 00:42:12 +0000

    Mon Apr 01 00:42:12 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -19,13 +19,33 @@ public class CheckoutSolution {
 	else if (x.equals("D")) total+=15;
 	}
 	int aPrice=0;
-	if(countA%3 ==0){aPrice=(countA/3)*130;}
-	 
+	if(countA>0){
+	if(countA<3){aPrice=countA*50;}
+	else if(countA>=3 && countA%3 ==0){aPrice=(countA/3)*130;}
+	else if(countA>3 && countA%3!=0) {
+		int quo= countA/3;
+		int rem= countA%3;
+		aPrice= (quo*130)+ rem*50;
+	}
+	total+=aPrice;
 	
+	int bPrice=0;
+	if(countB>0){
+		if(countB<2){bPrice=countB*30;}
+		else if(countB>=2 && countB%2 ==0){bPrice=(countB/2)*45;}
+		else if(countB>2 && countB%2!=0) {
+			int quo= countB/2;
+			int rem= countB%2;
+			bPrice= (quo*45)+ rem*30;
+		}
+	
+	total+=bPrice;
+	}
 	return total;
 	};
     public int checkout(String skus) {
        return msg.checkOut(skus);
     }
 }
+
 
