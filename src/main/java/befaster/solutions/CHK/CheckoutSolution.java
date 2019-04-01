7142f2282ba525de41c35731e3d8fba7a69cdf36
sbx_user1051 <sbx_user1051@169.254.25.205>
2019-04-01 00:00:12 +0000

    Mon Apr 01 00:00:12 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -5,10 +5,25 @@ interface CheckoutInterface {
 }
 
 public class CheckoutSolution {
-	CheckoutInterface msg = (String x...) -> {if(x.equalsIgnoreCase("A")) return 50;
+	CheckoutInterface msg = (x) -> {
+	int countA=0;
+	int countB=0;
+	int total=0;
+	for (String y : x ) {
+	if (y.equals("A")) countA++;
+	else if (y.equals("B")) countB++;
+	}
+	
+	for (String z : x ) {
+	if (x.equals("C")) total+=20;
+	else if (x.equals("D")) total+=15;
+	}
+	int aPrice=0;
+	if(countA ==1 ){total +=50;}
+	
+	return total;
 	};
-    public String checkout(String skus) {
+    public int checkout(String skus) {
        return msg.checkOut(skus);
     }
 }
-
