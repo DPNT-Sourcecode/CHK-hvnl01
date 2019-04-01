f5b809213b50f8e0c63e20ac14dfdaa676a9a8d5
sbx_user1051 <sbx_user1051@169.254.25.205>
2019-04-01 01:03:49 +0000

    Mon Apr 01 01:03:49 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -12,18 +12,18 @@ public class CheckoutSolution {
 	
 	for (String y : x ) {
 	if (y.equals("A")) countA++;
-	else if (y.equals("B")) countB++;
+	if (y.equals("B")) countB++;
 	}
 	
 	for (String z : x ) {
-	if (x.equals("C")) total+=20;
-	else if (x.equals("D")) total+=15;
+	if (z.equals("C")) total+=20;
+	if (z.equals("D")) total+=15;
 	}
 	int aPrice=0;
 	if(countA>0){
 	if(countA<3){aPrice=countA*50;}
-	else if(countA>=3 && countA%3 ==0){aPrice=(countA/3)*130;}
-	else if(countA>3 && countA%3!=0) {
+	if(countA>=3 && countA%3 ==0){aPrice=(countA/3)*130;}
+	if(countA>3 && countA%3!=0) {
 		int quo= countA/3;
 		int rem= countA%3;
 		aPrice= (quo*130)+ rem*50;
@@ -33,8 +33,8 @@ public class CheckoutSolution {
 	int bPrice=0;
 	if(countB>0){
 		if(countB<2){bPrice=countB*30;}
-		else if(countB>=2 && countB%2 ==0){bPrice=(countB/2)*45;}
-		else if(countB>2 && countB%2!=0) {
+		if(countB>=2 && countB%2 ==0){bPrice=(countB/2)*45;}
+		if(countB>2 && countB%2!=0) {
 			int quo= countB/2;
 			int rem= countB%2;
 			bPrice= (quo*45)+ rem*30;
@@ -48,3 +48,4 @@ public class CheckoutSolution {
        return msg.checkOut(skus);
     }
 }
+
