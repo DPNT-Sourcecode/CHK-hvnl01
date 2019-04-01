ff1af5756fb5362bb1dce5083c732caa2e1b16e1
sbx_user1051 <sbx_user1051@169.254.3.13>
2019-04-01 17:37:56 +0000

    Mon Apr 01 17:37:56 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -1,5 +1,8 @@
 package befaster.solutions.CHK;
 
+import java.util.HashMap;
+import java.util.regex.Pattern;
+
 interface CheckoutInterface {
 	int checkOut(String val);
 }
@@ -7,22 +10,39 @@ interface CheckoutInterface {
 public class CheckoutSolution {
 	
 	CheckoutInterface msg = (x) -> {
-		int countA=0;
-		int countB=0;
 		int total=0;
+		int aPrice=0;
+		int bPrice=0;
+	String regex = "^[A-D]+$";
+	boolean match = Pattern.matches(regex,x);
+	if(match){
+		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+		for(int i = 0; i< x.length(); i++){
+			char c = x.charAt(i);
+			Integer val = map.get(c);
+			if(val != null) {
+				map.put(c, new Integer(val + 1));
+			} else {
+				map.put(c, 1);
+			}
+		}
+		
+	int countA= map.get('A');
+	int countB=map.get('B');
+	if(map.get('A')<3){
+		aPrice=countA*50;
+	} else {
+	    if(countA>=3 && countA%3 ==0) {
+		  aPrice=(countA/3)*130;}
+	    }
+	    if(countA>3 && countA%3!=0) {
+		  int quo= countA/3;
+		  int rem= countA%3;
+		  aPrice= (quo*130)+ rem*50;
+	    }
+	}
 	
-	int aPrice=0;
-	if(countA>0){
-	if(countA<3){aPrice=countA*50;}
-	if(countA>=3 && countA%3 ==0){aPrice=(countA/3)*130;}
-	if(countA>3 && countA%3!=0) {
-		int quo= countA/3;
-		int rem= countA%3;
-		aPrice= (quo*130)+ rem*50;
-	}
-	total+=aPrice;
-	}
-	int bPrice=0;
+	
 	if(countB>0){
 		if(countB<2){bPrice=countB*30;}
 		if(countB>=2 && countB%2 ==0){bPrice=(countB/2)*45;}
@@ -34,20 +54,11 @@ public class CheckoutSolution {
 	
 	total+=bPrice;
 	}
-	
-	int blackList=0;
-	/*for(String p : x){
-		if(!(p.equals("A")||p.equals("B")||p.equals("C")||p.equals("D")||p.equals(""))){
-			blackList++;
-		}
-	}*/
-	if(blackList>0){
-		total=-1;
-	}
 	return total;
 	};
     public int checkout(String skus) {
        return msg.checkOut(skus);
     }
 }
+
 
