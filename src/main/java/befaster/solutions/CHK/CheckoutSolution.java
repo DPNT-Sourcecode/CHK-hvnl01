0deaf8f2d2174114b57caed7e07f9cbdcfd8ccc1
sbx_user1051 <sbx_user1051@169.254.127.157>
2019-04-02 20:58:02 +0000

    Tue Apr 02 20:58:02 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -18,7 +18,6 @@ public class CheckoutSolution {
     int pPrice=0;
     int qPrice=0;
     int vPrice=0;
-    int	remVal=0;
     String regex = "^[A-Z]+$";
     boolean match = Pattern.matches(regex,x);
     if(match || x.length()==0){
@@ -99,136 +98,23 @@ public class CheckoutSolution {
       
       //Cost of item A: A|50| 3A for 130, 5A for 200
       aPrice=getItemPrice(countA,50,3,130,5,200);
-      /*if(countA%5 !=0  ) {
-      	int quo= countA/5;
-          int rem= countA%5;
-          if(rem < 3) {
-            remVal= rem*50;
-          }else {
-             if(rem>3 && rem%3!=0) {
-                int quo2= rem/3;
-                int rem2= rem%3;
-                remVal= (quo2*130)+ rem2*50;
-              }
-             if(rem==3){
-               remVal=130;
-             }
-          }
-          aPrice= (quo*200)+ remVal;
-      }else {
-    	if(countA>=5  && countA%5 ==0  ) {
-          aPrice=(countA/5)*200;
-        }
-      }*/
-
-    //Cost of item B: B|30| 2B for 45 
+      
+      //Cost of item B: B|30| 2B for 45 
       bPrice=getItemPrice(countB,30,2,45);
-      /*if(countB<2) {
-        bPrice=countB*30;
-      }else {
-        if(countB >=2 && countB%2 ==0){
-          bPrice=(countB/2)*45;
-        }
-        if(countB>2 && countB%2!=0) {
-          int quo= countB/2;
-          int rem= countB%2;
-          bPrice= (quo*45)+ rem*30;
-        }
-      }*/
-
-    
       
       //Cost of H items :H|10|  5H for 45, 10H for 80
       hPrice=getItemPrice(countH,10,5,45,10,80);
-      /*if(countH%10 !=0  ) {
-        	int quo= countH/10;
-            int rem= countH%10;
-            if(rem < 5) {
-              remVal= rem*10;
-            }else {
-               if(rem>5 && rem%5!=0) {
-                  int quo2= rem/5;
-                  int rem2= rem%5;
-                  remVal= (quo2*45)+ rem2*10;
-                }
-               if(rem==5){
-                 remVal=45;
-               }
-            }
-            hPrice= (quo*80)+ remVal;
-        }else {
-      	if(countH>=10  && countH%10 ==0  ) {
-            hPrice=(countH/10)*80;
-          }
-        }*/
       
       //Cost of K items: K|80| 2K for 150
       kPrice=getItemPrice(countK,80,2,150);
-      /*if(countK<2) {
-          kPrice=countK*80;
-        }else {
-          if(countK >=2 && countK%2 ==0){
-            kPrice=(countK/2)*150;
-          }
-          if(countK>2 && countK%2!=0) {
-            int quo= countK/2;
-            int rem= countK%2;
-            kPrice= (quo*150)+ rem*80;
-          }
-        }*/
       
       //Cost of P item : P|50| 5P for 200
       pPrice=getItemPrice(countP,50,5,200);
-      /*if(countP<5) {
-          pPrice=countP*50;
-        }else {
-          if(countP >=5 && countP%5 ==0){
-            pPrice=(countP/5)*200;
-          }
-          if(countP>5 && countP%5!=0) {
-            int quo= countP/5;
-            int rem= countP%5;
-            pPrice= (quo*200)+ rem*50;
-          }
-        }*/
       
       //Cost of Q item: Q|30| 3Q for 80
       qPrice=getItemPrice(countQ,30,3,80);
-      /*if(countQ<3) {
-          qPrice=countQ*30;
-        }else {
-          if(countQ >=3 && countQ%3 ==0){
-            qPrice=(countQ/3)*80;
-          }
-          if(countQ>3 && countQ%3!=0) {
-            int quo= countQ/3;
-            int rem= countQ%3;
-            qPrice= (quo*80)+ rem*30;
-          }
-        }*/
       
       //Cost of V item: V|50|   2V for 90, 3V for 130
-     /* if(countV%3 !=0  ) {
-        	int quo= countV/3;
-            int rem= countV%3;
-            if(rem < 2) {
-              remVal= rem*50;
-            }else {
-               if(rem>2 && rem%2!=0) {
-                  int quo2= rem/2;
-                  int rem2= rem%2;
-                  remVal= (quo2*90)+ rem2*50;
-                }
-               if(rem==2){
-                 remVal=90;
-               }
-            }
-            vPrice= (quo*130)+ remVal;
-        }else {
-      	if(countV>=3  && countV%3 ==0  ) {
-            vPrice=(countV/3)*130;
-          }
-        }*/
       vPrice=getItemPrice(countV,50,2,90,3,130);
       
       total = aPrice + bPrice + countC*20 + countD*15+ countE*40+ countF*10+ countG*20+hPrice+ countI*35 + countJ*60+ +kPrice+ countL*90
@@ -239,7 +125,6 @@ public class CheckoutSolution {
     }
   };
   
-  //V|50|   2V for 90, 3V for 130
   public int getItemPrice(int count,int value,int firstCond, int value1, int secCond, int value2){
 	  int remVal=0;
 	  int price=0;
@@ -269,7 +154,6 @@ public class CheckoutSolution {
   
   public int getItemPrice(int count, int value, int firstCond, int value1){
 	  int price =0;
-	//Cost of Q item: Q|30| 3Q for 80
       if(count<firstCond) {
           price=count*value;
         }else {
@@ -289,4 +173,5 @@ public class CheckoutSolution {
     return value.checkOut(skus);
   }
 }
+
 
