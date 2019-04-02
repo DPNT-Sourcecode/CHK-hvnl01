45e4f4919a1e585ecca20c20fee86bdf94d489b6
sbx_user1051 <sbx_user1051@169.254.127.157>
2019-04-02 20:28:15 +0000

    Tue Apr 02 20:28:15 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -225,7 +225,7 @@ public class CheckoutSolution {
         }
       
       total = aPrice + bPrice + countC*20 + countD*15+ countE*40+ countF*10+ countG*20+hPrice+ countI*35 + countJ*60+ +kPrice+ countL*90
-    		  + countM*15 +countN*10+ countO*10 +pPrice+qPrice+ countR*50+countS*30 + countT*20 +countU*40 +vPrice+countW*20 + countX*90 + countY*10 + countZ*50 ;
+    		  + countM*15 +countN*40+ countO*10 +pPrice+qPrice+ countR*50+countS*30 + countT*20 +countU*40 +vPrice+countW*20 + countX*90 + countY*10 + countZ*50 ;
       return total;
     } else {
       return -1;  
@@ -239,3 +239,4 @@ public class CheckoutSolution {
     return value.checkOut(skus);
   }
 }
+
