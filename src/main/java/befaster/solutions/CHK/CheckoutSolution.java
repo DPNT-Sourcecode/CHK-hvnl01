5cbbe57f21ee63ff105c63abd22f49051eddb64a
sbx_user1051 <sbx_user1051@169.254.1.5>
2019-04-03 08:36:19 +0000

    Wed Apr 03 08:36:19 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -81,7 +81,7 @@ public class CheckoutSolution {
       if(countComb>2 && countComb%3==0){
         for(char l : comb){
         	if(l=='S'||l=='T'||l=='X'||l=='Y'||l=='Z'){
-        map.put(l,map.get(l)-1);
+        map.put(l,map.get(l)-1<0?0:map.get(l)-1);
         	}
         }       
       }
@@ -274,6 +274,7 @@ public class CheckoutSolution {
 	  System.out.println(v.checkout("AAAAAPPPPPUUUUEEBRRRQAAAHHHHHHHHHHKKVVVBBNNNMFFFQQQVVHHHHHSTX"));
   }
 }
+
 
 
 
