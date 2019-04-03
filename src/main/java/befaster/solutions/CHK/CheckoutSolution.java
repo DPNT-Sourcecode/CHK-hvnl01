7d38005d9b8cafb5bcbf55d52238e4c7bf5bcf99
sbx_user1051 <sbx_user1051@169.254.1.5>
2019-04-03 08:13:47 +0000

    Wed Apr 03 08:13:47 UTC 2019

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -57,7 +57,9 @@ public class CheckoutSolution {
       
       Map<Character,Integer> map2 = new HashMap<>();
       for(int i=0;i<comb.length;i++){
+    	  if(comb[i]!=' '){
     	  map2.put(comb[i],map1.get(comb[i]) );
+    	  }
       }
       
      /* HashMap<Character,Integer> sortMap = sortHashMapByValue((HashMap<Character, Integer>) map2);
@@ -269,6 +271,7 @@ public class CheckoutSolution {
 	  System.out.println(v.checkout("AAAAAPPPPPUUUUEEBRRRQAAAHHHHHHHHHHKKVVVBBNNNMFFFQQQVVHHHHHSTX"));
   }
 }
+
 
 
 
