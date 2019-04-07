a530f44a7793e367d48d4a50d0ac8273816207e2
sbx_user1051 <sbx_user1051@169.254.250.213>
2019-04-07 14:09:27 +0000

    Sun Apr 07 14:09:27 UTC 2019

--- src/main/java/befaster/solutions/CHK/ItemBasket.java
+++ src/main/java/befaster/solutions/CHK/ItemBasket.java
@@ -1,5 +1,29 @@
 package befaster.solutions.CHK;
 
-public class ItemBasket {
+import java.util.jar.Attributes.Name;
+
+public class ItemBasket implements Comparable<ItemBasket>{
+    String item;
+    double price;
+	
+	public ItemBasket(String item, double price) {
+		super();
+		this.item = item;
+		this.price = price;
+	}
+	@Override
+	public int compareTo(ItemBasket o) {
+		// TODO Auto-generated method stub
+		if(o == null) {
+			return -1;
+		}
+		int c = Double.valueOf(price).compareTo(o.price);
+		if(c!=0){
+			return c;
+		}
+			
+		return item.compareTo(o.item);
+	}
 
 }
+
